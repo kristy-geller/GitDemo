be541268bf6e398c46736f354e3ec024d2823d86
kristy-geller <easylifespan@gmail.com>
2022-01-06 11:31:53 -0500

    Final commit by asian guy

---
+++ src/test/java/qaclickacademy/demo.java
@@ -0,0 +1,13 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+		System.out.println("webAutomation");
+
+	}
+}
